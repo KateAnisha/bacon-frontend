dea337acdfef362e832c4f7f415cd8de0a4de6fe
kate.anisha <kate.anisha@icloud.com>
2021-07-24 11:14:03 +1000

    made changes to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,23 +8,37 @@ function Home() {
             <h1> App home page </h1>
             <div className="home-wrapper">
                 <div className="home-content">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam perferendis dolorum incidunt non et, accusamus, praesentium voluptates nam, in veritatis optio. Ipsa eveniet obcaecati id, non nesciunt corrupti commodi assumenda.
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam perferendis dolorum incidunt non et, accusamus, praesentium voluptates nam, in veritatis optio. Ipsa eveniet obcaecati id, non nesciunt corrupti commodi assumenda.
+                    </p>
                 </div>
                 
                 <div className="home-content">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, ab dolores? Asperiores ipsam dolorem exercitationem quidem fugiat, expedita deleniti ex aperiam deserunt saepe minus, maxime optio atque tempora beatae ducimus?
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam perferendis dolorum incidunt non et, accusamus, praesentium voluptates nam, in veritatis optio. Ipsa eveniet obcaecati id, non nesciunt corrupti commodi assumenda.
+                    </p>
                 </div>
 
                 <div className="home-content">
                     <div className="nested">
-                        <div className="sub-menu">Manage Budget</div>
-                        <div className="sub-menu">Manage Accounts</div>
-                        <div className="sub-menu">User Settings</div>
+                        <div className="container">
+                            <div className="sub-menu">Manage Budget</div> <div className="arrow"></div>
+                        </div>
+
+                        <div className="container">
+                            <div className="sub-menu">Manage Categories</div> <div className="arrow"></div>
+                        </div>
+
+                        <div className="container">
+                            <div className="sub-menu">User Settings</div> <div className="arrow"></div>
+                        </div>
                     </div>
                 </div>
 
                 <div className="charts">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, ab dolores? Asperiores ipsam dolorem exercitationem quidem fugiat, expedita deleniti ex aperiam deserunt saepe minus, maxime optio atque tempora beatae ducimus?
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam perferendis dolorum incidunt non et, accusamus, praesentium voluptates nam, in veritatis optio. Ipsa eveniet obcaecati id, non nesciunt corrupti commodi assumenda.
+                    </p>
                 </div>
             </div>
         </>
